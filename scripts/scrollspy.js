a29ce8aab53ed91f27598443a394b5eeed96d833
Eriza Zehezkiel Gracia <nftrgaming@gmail.com>
2022-06-05 08:31:50 +0700

    Uhhhh

--- scripts/scrollspy.js
+++
@@ -1,3 +0,0 @@
-var scrollSpy = new bootstrap.ScrollSpy(document.body, { target: '#navbar' })
-var dataSpyList = [].slice.call(document.querySelectorAll('[data-bs-spy="scroll"]'))
-dataSpyList.forEach(function (dataSpyEl) { bootstrap.ScrollSpy.getInstance(dataSpyEl).refresh() })
