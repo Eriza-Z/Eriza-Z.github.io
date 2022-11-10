8baf56d2b82964c5ccd3c474c8ebf6abae0d6731
Eriza Zehezkiel Gracia <nftrgaming@gmail.com>
2022-11-10 08:47:01 +0700

    Changing from <img> to <Image/>

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['cdn.discordapp.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        port: '443',
+        pathname: 'attachments/971801947910848582/1019433060028129390/IMG_20220903_183158.jpg',
+      },
+    ],
+    formats: ['image/webp'],
+  },
 }
 
 module.exports = nextConfig
