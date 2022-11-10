6787b0276e23abc45bc06354afa7dcf55a027193
Eriza Zehezkiel Gracia <nftrgaming@gmail.com>
2022-11-10 14:48:18 +0700

    .

--- next.config.js
+++ next.config.js
@@ -4,21 +4,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    urlImports: ['https://cdn.discordapp.com/attachments/'],
-  },
-  // images: {
-  //   domains: ['cdn.discordapp.com'],
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'cdn.discordapp.com',
-  //       port: '443',
-  //       pathname: 'attachments/971801947910848582/1019433060028129390/IMG_20220903_183158.jpg',
-  //     },
-  //   ],
-  //   formats: ['image/webp'],
-  // },
 }
 
 module.exports = nextConfig
