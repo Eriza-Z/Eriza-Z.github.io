583c81eaf87b16eafe42279866a2d39209be2ff2
Eriza Zehezkiel Gracia <75519646+Eriza-Z@users.noreply.github.com>
2023-01-22 21:05:41 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,10 +32,10 @@ export default function Home() {
         <meta charSet='UTF-8'/>
         <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
         {/* OpenGraph */}
-        <meta property="og:site_name" content="Eriza Zehezkiel Gracia" />
-        <meta property="og:title" content="Eriza Zehezkiel Gracia" />
+        <meta property="og:site_name" content="ErZ" />
+        <meta property="og:title" content="ErZ" />
         <meta property="og:type" content="website" />
-        <meta property="og:description" content="Personal Website of Eriza Zehezkiel Gracia." />
+        <meta property="og:description" content="Personal Website of ErZ." />
         <meta property="og:url" content="https://erizazg.my.id" />
         <meta property="og:image" content="http://raw.githubusercontent.com/Eriza-Z/Eriza-Z.github.io/master/Web/Icon/icon.webp" />
         <meta property="og:image:secure_url" content="https://raw.githubusercontent.com/Eriza-Z/Eriza-Z.github.io/master/Web/Icon/icon.webp" />
@@ -49,8 +49,8 @@ export default function Home() {
         <meta name="twitter:site" content="@eriza_gracia" />
         <meta name="twitter:creator" content="@eriza_gracia" />
         <meta name="twitter:url" content="https://erizazg.my.id" />
-        <meta name="twitter:title" content="Eriza Zehezkiel Gracia" />
-        <meta name="twitter:description" content="Personal Website of Eriza Zehezkiel Gracia." />
+        <meta name="twitter:title" content="ErZ" />
+        <meta name="twitter:description" content="Personal Website of ErZ." />
         <meta name="twitter:image" content="https://raw.githubusercontent.com/Eriza-Z/Eriza-Z.github.io/master/Web/Icon/icon.webp" />
         <meta name="twitter:wigets:theme" content="dark" />
         {/* Apple */}
@@ -60,7 +60,7 @@ export default function Home() {
         {/* Website Thingy */}
         <title>ErZ</title>
         <meta name="keywords" content="Eriza Zehezkiel, Eriza Z, Eriza Zehezkiel Gracia, EZGS, ezgs, ErZ" />
-        <meta name="description" content="Personal Website of Eriza Zehezkiel Gracia where this Web page Layout is like a Portofolio Website Page Layout" />
+        <meta name="description" content="Nothing" />
         <meta name="author" content="Eriza Zehezkiel Gracia" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
