4fef1f45fdb8de412e8e638e8c864513498aa26b
Eriza Zehezkiel Gracia <nftrgaming@gmail.com>
2023-03-05 17:17:02 +0700

    Adding metadata to password generator

--- pages/password-generator.tsx
+++ pages/password-generator.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React, { useState } from "react";
 import { toast } from "react-hot-toast";
 import { FaClipboard } from "react-icons/fa";
@@ -64,6 +65,60 @@ function App() {
 
   return (
     <section>
+      <Head>
+        {/* Web Property */}
+        <title>ErZ - Password Generator</title>
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width,minimum-scale=1,initial-scale=1"
+        />
+        <meta
+          name="keywords"
+          content="Eriza Zehezkiel, Eriza Z, Eriza Zehezkiel Gracia, EZGS, ezgs, Password Generator, Password generator"
+        />
+        <meta
+          name="description"
+          content="Eriza Zehezkiel Gracia Password Generator"
+        />
+        <meta name="author" content="Eriza Zehezkiel Gracia" />
+        <link rel="icon" href="/favicon.ico" />
+        {/* OpenGraph */}
+        <meta property="og:site_name" content="ErZ" />
+        <meta property="og:title" content="ErZ" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Password Generator Website Page from ErZ." />
+        <meta property="og:url" content="https://erizazg.my.id" />
+        <meta
+          property="og:image"
+          content="http://raw.githubusercontent.com/Eriza-Z/Eriza-Z.github.io/master/public/icon.jpg"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://raw.githubusercontent.com/Eriza-Z/Eriza-Z.github.io/master/public/icon.jpg"
+        />
+        <meta property="og:image:type" content="image/webp" />
+        <meta property="og:image:width" content="256" />
+        <meta property="og:image:height" content="256" />
+        <meta property="og:image:alt" content="Foto lorong kelas" />
+        <meta name="theme-color" content="#080452" />
+        {/* Twitter Cards */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@eriza_gracia" />
+        <meta name="twitter:creator" content="@eriza_gracia" />
+        <meta name="twitter:url" content="https://erizazg.my.id" />
+        <meta name="twitter:title" content="ErZ" />
+        <meta name="twitter:description" content="Password Generator Website Page from ErZ." />
+        <meta
+          name="twitter:image"
+          content="https://raw.githubusercontent.com/Eriza-Z/Eriza-Z.github.io/master/public/icon.jpg"
+        />
+        <meta name="twitter:wigets:theme" content="dark" />
+        {/* Apple */}
+        <link rel="apple-touch-icon" href="../public/apple-touch-icon.png" />
+        {/* Canonical */}
+        <link rel="canonical" href="https://erizazg.my.id/password-generator" />
+      </Head>
       <div className={style.container}>
         <div className={style.border}>
           <form id="pg-form" onSubmit={handleOnSubmit}>
