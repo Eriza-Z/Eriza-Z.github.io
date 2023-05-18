7f1d4e0e4fc31148f8bd4e87ea69e9804fd5c425
Eriza Zehezkiel Gracia <nftrgaming@gmail.com>
2023-05-18 13:30:13 +0700

    Adding some href

--- Components/Other/Footer.js
+++ Components/Other/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div>
       <div className="text-white tengah">
-        Copyright © 2023 Eriza Zehezkiel Gracia
+        Copyright © 2023 <a href="https://instagram.com/ezgrxs" rel="noopenner noreferrer">Eriza Zehezkiel Gracia</a>
         <br />
         <Link href={"/about"}>About</Link> | <Link href={"/link"}>Link</Link> | <Link href={"/Gallery"}>Gallery</Link> | <Link href={"/technology"}>Technology</Link><br/>
         Mail me at: <a href="mailto:erz@erizazg.my.id">erz@erizazg.my.id</a>
