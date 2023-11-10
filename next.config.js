7e75327243e2cdd1489da643de894469e50ca293
Eriza Zehezkiel Gracia <erz@erizazg.my.id>
2023-11-11 06:12:09 +0700

    Updating due to next export command error

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
     domains: ['cdn.discordapp.com', 'mergoreb.sirv.com', 'lh3.googleusercontent.com', 'source.unsplash.com'],
     unoptimized: true,
   },
+  output: 'export'
 }
 
 module.exports = nextConfig
