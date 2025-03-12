a9d42e790886d8ffc23fcda628c0c61df17eefd8
Eriza Zehezkiel Gracia <erz@erizazg.my.id>
2025-03-12 23:27:42 +0700

    Fixing content

--- Components/Page/Belajar.js
+++ Components/Page/Belajar.js
@@ -4,7 +4,6 @@ import style from "../../styles/Home.module.scss";
 function Belajar() {
   return (
     <div className='grid place-items-center'>
-        <h2 className='mt-2 mb-2.5'>Others</h2>
         <div className={style.button_area}>
           <a
             href="https://drive.google.com/drive/folders/1rYY_M0XxqijFuSNoGeY7o8rTrf1PdD3q?usp=sharing"
